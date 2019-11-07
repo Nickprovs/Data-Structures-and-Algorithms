eb19fdfaa1a0dcb696e0fc8855cc0d489f92fa57
Nick Provost <nickprovs@hotmail.com>
2019-11-06 22:53:39 -0500

    Dijkstra's Shortest Path Algorithm.

--- NonLinear/NonLinearDemos/Demos/WeightedGraphDemo.cs
+++ NonLinear/NonLinearDemos/Demos/WeightedGraphDemo.cs
@@ -14,10 +14,19 @@ namespace NonLinearExperimentation.Demos
             weightedGraphA.AddNode("A");
             weightedGraphA.AddNode("B");
             weightedGraphA.AddNode("C");
-            weightedGraphA.AddEdge("A", "B", 3);
-            weightedGraphA.AddEdge("A", "C", 2);
+            weightedGraphA.AddNode("D");
+            weightedGraphA.AddNode("E");
+
+            weightedGraphA.AddEdge("A", "B", 1);
+            weightedGraphA.AddEdge("B", "C", 1);
+            weightedGraphA.AddEdge("B", "E", 12);
+            weightedGraphA.AddEdge("C", "D", 1);
+            weightedGraphA.AddEdge("D", "E", 1);
+            weightedGraphA.AddEdge("A", "E", 10);
 
             weightedGraphA.Print();
+
+            Console.WriteLine($"Shortest path from A to E: {string.Join(",", weightedGraphA.GetShortestPath("A", "E"))}");
         }
     }
 }
--- NonLinear/NonLinearLibrary/WeightedGraph.cs
+++ NonLinear/NonLinearLibrary/WeightedGraph.cs
@@ -18,7 +18,7 @@ namespace NonLinearLibrary
             public override string ToString()
             {
                 return this.Label;
-            }           
+            }
             public void AddEdge(Node to, int weight)
             {
                 this.Edges.Add(new Edge(this, to, weight));
@@ -46,12 +46,35 @@ namespace NonLinearLibrary
             }
         }
 
+        private class NodeEntry
+        {
+            internal Node Node;
+            internal int Priority;
+
+            public NodeEntry(Node node, int priority)
+            {
+                this.Node = node;
+                this.Priority = priority;
+            }
+        }
+        private class NodeEntryComparer : IComparer<NodeEntry>
+        {
+            public int Compare(NodeEntry x, NodeEntry y)
+            {
+                if (x.Priority > y.Priority)
+                    return -1;
+                if (x.Priority < y.Priority)
+                    return 1;
+                else
+                    return 0;
+            }
+        }
 
         private Dictionary<string, Node> _nodes;
 
         public WeightedGraph()
         {
-            this._nodes = new Dictionary<string, Node>(); 
+            this._nodes = new Dictionary<string, Node>();
         }
         public void AddNode(string label)
         {
@@ -81,6 +104,71 @@ namespace NonLinearLibrary
                 if (targets.Count > 0)
                     Console.WriteLine($"{node} is connected to: {string.Join(",", targets)}");
             }
+        }
+
+        public List<string> GetShortestPath(string from, string to)
+        {
+            Dictionary<Node, int> distances = new Dictionary<Node, int>();
+            foreach (var node in this._nodes.Values)
+                distances.Add(node, int.MaxValue);
+
+            var fromNode = this._nodes[from];
+            if (fromNode == null)
+                throw new ArgumentException(nameof(from));
+
+            var toNode = this._nodes[to];
+            if (toNode == null)
+                throw new ArgumentException(nameof(to));
+
+            distances[fromNode] = 0;
+
+            Dictionary<Node, Node> previousNodes = new Dictionary<Node, Node>();
+
+            HashSet<Node> visited = new HashSet<Node>();
+
+            SortedSet<NodeEntry> queue = new SortedSet<NodeEntry>(new NodeEntryComparer());
+            queue.Add(new NodeEntry(fromNode, 0));
+
+            while (queue.Count > 0)
+            {
+                var current = queue.Max.Node;
+                queue.Remove(queue.Max);
+
+                visited.Add(current);
+                foreach (var edge in current.GetEdges())
+                {
+                    if (visited.Contains(edge.To))
+                        continue;
+
+                    var newDistance = distances[current] + edge.Weight;
+                    if (newDistance < distances[edge.To])
+                    {
+                        distances[edge.To] = newDistance;
+                        previousNodes[edge.To] = current;
+                        queue.Add(new NodeEntry(edge.To, newDistance));
+                    }
+                }
+            }
+
+            return this.BuildPath(toNode, previousNodes);
+        }
+
+        private List<string> BuildPath(Node toNode, Dictionary<Node, Node> previousNodes)
+        {
+            Stack<Node> stack = new Stack<Node>();
+            stack.Push(toNode);
+            previousNodes.TryGetValue(toNode, out var previous);
+            while (previous != null)
+            {
+                stack.Push(previous);
+                previousNodes.TryGetValue(previous, out previous);
+            }
+
+            List<string> path = new List<string>();
+            while (stack.Count > 0)
+                path.Add(stack.Pop().Label);
+
+            return path;
         }
     }
 }
