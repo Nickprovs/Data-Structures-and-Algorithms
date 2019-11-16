f96eca5575b419f1db366d2d6dbd345c5e9ff8a3
Nick Provost <nickprovs@hotmail.com>
2019-11-15 19:27:01 -0500

    Ternary search

--- Part 3 - Common Algorithms/CommonAlgorithmsLibrary/Searching/TernarySearchExtension.cs
+++ Part 3 - Common Algorithms/CommonAlgorithmsLibrary/Searching/TernarySearchExtension.cs
@@ -13,7 +13,26 @@ namespace CommonAlgorithmsLibrary.Searching
 
         private static int TernarySearch(IList<int> list, int target, int left, int right)
         {
-            return -1;
+            if (left > right)
+                return -1;
+
+            int partitionSize = (right - left) / 3;
+            int mid1 = left + partitionSize;
+            int mid2 = right - partitionSize;
+
+            if (list[mid1] == target)
+                return mid1;
+
+            if (list[mid2] == target)
+                return mid2;
+
+            if (target < list[mid1])
+                return TernarySearch(list, target, left, mid1 - 1);
+
+            if (target > list[mid2])
+                return TernarySearch(list, target, mid2 + 1, right);
+
+            return TernarySearch(list, target, mid1 + 1, mid2 - 1);
         }
     }
 }
